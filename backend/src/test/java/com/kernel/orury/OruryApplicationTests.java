2fb4a2fc5635f2de0872451b7a78dc8a6b6a6d1a
Hyungjoon Cho <selcon1996@gmail.com>
2023-10-24 14:48:42 +0900

    [Feat] #1. 프로젝트 초기세팅

---
+++ backend/src/test/java/com/kernel/orury/OruryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel.orury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OruryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/android/app/src/main/java/com/kernel/frontend/MainActivity.java
@@ -0,0 +1,7 @@
+package com.kernel.frontend;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}

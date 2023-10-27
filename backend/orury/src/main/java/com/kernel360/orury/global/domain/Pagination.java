fa826a56f40531cfca224e2fedba69f39eb100ce
ShineCorine <imsjm81@gmail.com>
2023-10-27 23:22:20 +0900

    [Feat] #42. Pagination 완성

--- backend/orury/src/main/java/com/kernel360/orury/global/domain/Pagination.java
+++ backend/orury/src/main/java/com/kernel360/orury/global/domain/Pagination.java
@@ -1,2 +1,18 @@
-package com.kernel360.orury.global.domain;public class Pagination {
+package com.kernel360.orury.global.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Pagination {
+
+    private Integer page;
+    private Integer size;
+    private Integer currentElements;
+    private Integer totalPage;
+    private Long totalElements;
 }

31ea11bdbc344f55ba3a25da6c6ad7eb65e85a45
Hyungjoon Cho <selcon1996@gmail.com>
2023-11-02 23:58:25 +0900

    [Feat] #51. 회원가입 및 로그인 화면 개발
    - 기존 로그인 화면 교체, 안정성 검증 후 기존 로그인 화면 파일 삭제 예정
    - 회원가입 및 로그인 시 email 유효 및 비밀번호 규칙 검증로직 추가

---
+++ frontend/lib/global/messages/user/user_message.dart
@@ -0,0 +1,11 @@
+class UserMessage {
+  static const String enterYourPassword = "비밀번호를 입력해주세요.";
+  static const String reEnterYourPassword = "비밀번호를 확인해주세요.";
+  static const String misMatchPassword = "비밀번호를 확인해주세요.";
+  static const String enterYourEmail = "이메일을 입력해주세요.";
+  static const String enterYourNickname = "닉네임을 입력해주세요.";
+  static const String invalidEmailForm = "올바른 이메일 형식이 아닙니다.";
+  static const String invalidNicknameForm = "닉네임은 2글자 이상이어야 합니다.";
+  static const String invalidPasswordForm = "비밀번호는 영어와 특수문자를 혼합한 8자 이상이어야 합니다.";
+
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter/services.dart';
 import 'package:orury/core/theme/theme_data.dart';
-import 'package:orury/presentation/pages/home/home_page.dart';
-import 'package:orury/presentation/pages/splash/splash_page.dart';
-// import 'package:orury/presentation/pages/user/SettingPage.dart';
-import 'package:orury/presentation/pages/user/user_page.dart';
+import 'package:orury/presentation/pages/user/login_page.dart';
+import 'package:orury/presentation/pages/user/register_page.dart';
 import 'package:orury/presentation/routes/routes.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:orury/values/app_constants.dart';
+import 'package:orury/values/app_routes.dart';
+import 'package:orury/values/app_theme.dart';
 
 void main() async{
   await dotenv.load(fileName: 'lib/global/config/.env');
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  ).then(
+        (_) => runApp(
+      const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,9 +32,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // return MaterialApp.router(
+    //   routerConfig: router,
+    // );
+    Theme.of(context);
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Login and Register UI',
+      theme: AppTheme.themeData,
       routerConfig: router,
-      theme: CustomThemeData.themeData,
+      // initialRoute: AppRoutes.loginScreen,
+      // navigatorKey: AppConstants.navigationKey,
+      // routes: {
+      //   AppRoutes.loginScreen: (context) => const LoginPage(),
+      //   AppRoutes.registerScreen: (context) => const RegisterPage(),
+      // },
     );
   }
 }

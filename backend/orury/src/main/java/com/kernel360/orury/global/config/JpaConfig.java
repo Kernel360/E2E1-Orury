ad046b97642bc9271d9a4a942983c04fb1022ac0
Hyungjoon Cho <selcon1996@gmail.com>
2023-11-02 12:58:43 +0900

    [Refactor] #63. 코드리뷰 사항 반영
    - "admin" 상수처리
    - 누락된 주석 추가

--- backend/orury/src/main/java/com/kernel360/orury/global/config/JpaConfig.java
+++ backend/orury/src/main/java/com/kernel360/orury/global/config/JpaConfig.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author : hyungjoon cho
- * @date :
+ * @date : 2023/11/02
+ * @description : 테스트코드의 JPA 설정을 담당하는 설정파일
  */
 @Configuration
 public class JpaConfig {

603548d5fe26dbf357b37c7ced60db457712b856
ShineCorine <imsjm81@gmail.com>
2023-11-15 15:38:52 +0900

    swager ui에 jwt 토큰을 입력하여 테스트 할 수 있도록 설정  추가

--- backend/orury/src/main/java/com/kernel360/orury/config/SwaggerConfig.java
+++ backend/orury/src/main/java/com/kernel360/orury/config/SwaggerConfig.java
@@ -1,2 +1,35 @@
-package com.kernel360.orury.config;public class SwaggerConfig {
+package com.kernel360.orury.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("API")
+                .description("");
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }

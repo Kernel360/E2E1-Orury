5aade9edf89f1463b5d0d5a34ab39825c8e07c40
aqrms <aquarimus@gmail.com>
2023-11-21 10:44:13 +0900

    [feat] #118. Apply gitignore to logfiles

--- backend/orury/src/main/java/com/kernel360/orury/config/SecurityConfig.java
+++ backend/orury/src/main/java/com/kernel360/orury/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
